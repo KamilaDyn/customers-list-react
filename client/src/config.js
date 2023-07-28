c50d998ba6a2da067356a4bb62efc3aa02bce71f
Kamila <kamila.dynysiuk@gmail.com>
2023-07-28 11:01:44 +0200

    change url

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,2 @@
-// export const baseUrl = 'https://customers-api-sgd5.onrender.com';
+export const baseUrl = "https://customers-api-sgd5.onrender.com";
 // export const baseUrl = "http://localhost:5000";
-export const baseUrl = "https://customer-list-react.onrender.com";
